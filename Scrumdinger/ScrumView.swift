c1cc0fac7eaf47d92c1808c64b7844025577644f
YamadaTakahito <d.p.t.a.d.fighter@gmail.com>
2021-06-06 14:19:02 +0900

    finish Displaying Data in a List

---
+++ Scrumdinger/ScrumView.swift
@@ -0,0 +1,10 @@
+//
+//  ScrumView.swift
+//  Scrumdinger
+//
+//  Created by 山田崇仁 on 2021/06/06.
+//
+
+import SwiftUI
+
+
---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by 山田崇仁 on 2021/06/06.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
